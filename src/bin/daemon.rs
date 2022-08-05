6ff43fcd9543acb79d6f4c343e102f5029f5480f
Antoine Poinsot <darosior@protonmail.com>
2022-08-05 18:24:32 +0200

    daemon: log the name of the current thread, too.

--- src/bin/daemon.rs
+++ src/bin/daemon.rs
@@ -2,7 +2,7 @@ use std::{
     env,
     io::{self, Write},
     path::PathBuf,
-    process, time,
+    process, thread, time,
 };
 
 use minisafe::{config::Config, DaemonHandle};
@@ -25,7 +25,7 @@ fn setup_logger(log_level: log::LevelFilter) -> Result<(), fern::InitError> {
     let dispatcher = fern::Dispatch::new()
         .format(|out, message, record| {
             out.finish(format_args!(
-                "[{}][{}][{}] {}",
+                "[{}][{}][{}][thread {}] {}",
                 time::SystemTime::now()
                     .duration_since(time::UNIX_EPOCH)
                     .unwrap_or_else(|e| {
@@ -35,6 +35,7 @@ fn setup_logger(log_level: log::LevelFilter) -> Result<(), fern::InitError> {
                     .as_secs(),
                 record.target(),
                 record.level(),
+                thread::current().name().unwrap_or("unnamed"),
                 message
             ))
         })
