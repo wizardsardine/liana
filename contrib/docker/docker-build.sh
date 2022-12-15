9a800113a713b1687960c14b77b1ada1df17bdff
Antoine Poinsot <darosior@protonmail.com>
2022-12-15 19:11:36 +0100

    contrib: reproducible builds for macOS using Docker
    The gist is: we use https://github.com/tpoechtrager/osxcross.
    
    This contains a few cleanups of the "Docker reproducible builds" folder
    as a whole.

--- contrib/docker/docker-build.sh
+++ contrib/docker/docker-build.sh
@@ -2,14 +2,35 @@
 
 set -ex
 
-TARGET_DIR="$PWD/deter_build_target"
+TARGET_DIR="${TARGET_DIR:-"$PWD/deter_build_target"}"
 
-docker build . -t liana_cross_compile -f contrib/docker/Dockerfile
+XCODE_PATH="${XCODE_PATH:-"$PWD/Xcode_12.2.xip"}"
+
+# Build (only) the Liana GUI on Windows.
+docker build . -t liana_cross_win -f contrib/docker/windows.Dockerfile
 docker run --rm -ti \
-    -v "$TARGET_DIR":/liana/target \
+    -v "$TARGET_DIR/gui":/liana/target \
     -v "$PWD/contrib/docker":/liana/docker \
     -v "$PWD/gui/src":/liana/src \
     -v "$PWD/gui/static":/liana/static \
-    liana_cross_compile
+    liana_cross_win
+
+# Sanity check the given MacOS SDK is the expected one.
+if ! $(echo "28d352f8c14a43d9b8a082ac6338dc173cb153f964c6e8fb6ba389e5be528bd0 $(basename $XCODE_PATH)" | sha256sum -c --status); then
+    echo "No or invalid Xcode SDK found. Need an Xcode_12.2.xip. You can configure the path using \$XCODE_PATH.";
+    exit 1;
+fi
+
+# Build both the Liana daemon and GUI on MacOS.
+docker build . -t liana_cross_mac -f contrib/docker/macos.Dockerfile
+docker run --rm -ti \
+    -v "$TARGET_DIR":/liana/target \
+    -v "$TARGET_DIR/gui":/liana/gui/target \
+    -v "$PWD/contrib/docker":/liana/docker \
+    -v "$PWD/src":/liana/src \
+    -v "$PWD/gui/src":/liana/gui/src \
+    -v "$PWD/gui/static":/liana/gui/static \
+    -v "$XCODE_PATH":/liana/Xcode_12.2.xip \
+    liana_cross_mac
 
 set +ex
--- contrib/docker/windows_cmd.sh
+++ contrib/docker/windows_cmd.sh
@@ -4,10 +4,17 @@
 # The script ran within the Docker container to build the Liana GUI.
 # =================================================================
 
+set -xe
 
 # Build the GUI for Windows. The Windows Portable Execution (PE) format contains some timestamps.
 # Instruct ld to set them to 0.
-RUSTFLAGS="-Clink-arg=-Wl,--no-insert-timestamp" /liana/rust-1.65.0-x86_64-unknown-linux-gnu/cargo/bin/cargo rustc --release --target x86_64-pc-windows-gnu
+alias cargo="/liana/rust-1.65.0-x86_64-unknown-linux-gnu/cargo/bin/cargo"
+RUSTFLAGS="-Clink-arg=-Wl,--no-insert-timestamp" \
+    cargo rustc \
+        --release \
+        --target x86_64-pc-windows-gnu
 
 # Avoid having to get root on the host to remove the target dir.
 chmod -R a+rw target/
+
+set +xe
