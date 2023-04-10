fe52fbaa49096d1b1beffee09d56f5cc8272a585
Antoine Poinsot <darosior@protonmail.com>
2023-04-10 11:58:51 +0200

    gui: bump to 0.4

--- contrib/reproducible/guix/build.sh
+++ contrib/reproducible/guix/build.sh
@@ -20,7 +20,7 @@ replace-with = "vendored_sources"
 
 [source."https://github.com/wizardsardine/liana"]
 git = "https://github.com/wizardsardine/liana"
-branch = "master"
+branch = "0.4-lianad"
 replace-with = "vendored_sources"
 EOF
 
