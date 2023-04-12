920e8bc3300beb0b89c9d93b9701e7eee8e79288
edouard <m@edouard.paris>
2023-04-12 11:59:02 +0200

    gui: change font for IBM Plex Sans

--- gui/ui/src/font.rs
+++ gui/ui/src/font.rs
@@ -2,10 +2,15 @@ use iced::Font;
 
 pub const BOLD: Font = Font::External {
     name: "Bold",
-    bytes: include_bytes!("../static/fonts/OpenSans-Bold.ttf"),
+    bytes: include_bytes!("../static/fonts/IBMPlexSans-Bold.ttf"),
+};
+
+pub const MEDIUM: Font = Font::External {
+    name: "Regular",
+    bytes: include_bytes!("../static/fonts/IBMPlexSans-Medium.ttf"),
 };
 
 pub const REGULAR: Font = Font::External {
     name: "Regular",
-    bytes: include_bytes!("../static/fonts/OpenSans-Regular.ttf"),
+    bytes: include_bytes!("../static/fonts/IBMPlexSans-Regular.ttf"),
 };
