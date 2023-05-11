1a1e0c245f208487dbec1ffda11c4ca0912e1610
Antoine Poinsot <darosior@protonmail.com>
2023-05-11 09:22:13 +0200

    contrib: make the Xcode archive configurable for MacOS builds

--- contrib/reproducible/docker/docker-build.sh
+++ contrib/reproducible/docker/docker-build.sh
@@ -5,6 +5,8 @@ set -ex
 TARGET_DIR="${TARGET_DIR:-"$PWD/deter_build_target"}"
 
 XCODE_PATH="${XCODE_PATH:-"$PWD/Xcode_12.2.xip"}"
+XCODE_FILENAME="$(basename $XCODE_PATH)"
+XCODE_SHASUM="28d352f8c14a43d9b8a082ac6338dc173cb153f964c6e8fb6ba389e5be528bd0"
 
 # Build (only) the Liana GUI on Windows.
 docker build . -t liana_cross_win -f contrib/reproducible/docker/windows.Dockerfile
@@ -22,14 +24,14 @@ docker run --rm -ti \
 
 
 # Sanity check the given MacOS SDK is the expected one.
-if ! $(echo "28d352f8c14a43d9b8a082ac6338dc173cb153f964c6e8fb6ba389e5be528bd0 $(basename $XCODE_PATH)" | sha256sum -c --status); then
-    echo "No or invalid Xcode SDK found. Need an Xcode_12.2.xip. You can configure the path using \$XCODE_PATH.";
+if ! $(echo "$XCODE_SHASUM $(basename $XCODE_PATH)" | sha256sum -c --status); then
+    echo "No or invalid Xcode SDK found. Need an Xcode_X.Y.xip archive whose hash is $XCODE_SHASUM. You can configure the path using \$XCODE_PATH.";
     exit 1;
 fi
 
 # Build both the Liana daemon and GUI on MacOS.
 docker build . -t liana_cross_mac -f contrib/reproducible/docker/macos.Dockerfile
-docker run --rm -ti \
+docker run -ti \
     -v "$TARGET_DIR":/liana/target \
     -v "$TARGET_DIR/gui":/liana/gui/target \
     -v "$PWD/contrib/reproducible/docker":/liana/docker \
@@ -43,7 +45,8 @@ docker run --rm -ti \
     -v "$PWD/gui/ui/Cargo.lock":/liana/gui/ui/Cargo.lock \
     -v "$PWD/gui/ui/src":/liana/gui/ui/src \
     -v "$PWD/gui/ui/static":/liana/gui/ui/static \
-    -v "$XCODE_PATH":/liana/Xcode_12.2.xip \
+    -v "$XCODE_PATH":"/liana/$XCODE_FILENAME" \
+    -e XCODE_PATH="/liana/$XCODE_FILENAME" \
     liana_cross_mac
 
 set +ex
