f19dc4627ee60e669411b4483e717f57ef69bbd4
edouardparis <m@edouard.paris>
2024-01-12 15:56:27 +0100

    ui: add processing hw notification to components

--- gui/ui/src/component/notification.rs
+++ gui/ui/src/component/notification.rs
@@ -1,10 +1,17 @@
+use std::borrow::Cow;
+use std::fmt::Display;
+
 use crate::{
     color,
     component::{collapse, text},
     icon, theme,
+    util::*,
     widget::*,
 };
-use iced::{Alignment, Length};
+use iced::{
+    widget::{column, container, row},
+    Alignment, Length,
+};
 
 pub fn warning<'a, T: 'a + Clone>(message: String, error: String) -> Container<'a, T> {
     let message_clone = message.clone();
@@ -14,7 +21,7 @@ pub fn warning<'a, T: 'a + Clone>(message: String, error: String) -> Container<'
                 Row::new()
                     .push(
                         Container::new(
-                            text::p1_bold(message_clone.to_string()).style(color::WHITE),
+                            text::p1_bold(message_clone.to_string()).style(color::LIGHT_BLACK),
                         )
                         .width(Length::Fill),
                     )
@@ -22,8 +29,8 @@ pub fn warning<'a, T: 'a + Clone>(message: String, error: String) -> Container<'
                         Row::new()
                             .align_items(Alignment::Center)
                             .spacing(10)
-                            .push(text::p1_bold("Learn more").style(color::WHITE))
-                            .push(icon::collapse_icon()),
+                            .push(text::p1_bold("Learn more").style(color::LIGHT_BLACK))
+                            .push(icon::collapse_icon().style(color::LIGHT_BLACK)),
                     ),
             )
             .style(theme::Button::Transparent)
@@ -32,15 +39,15 @@ pub fn warning<'a, T: 'a + Clone>(message: String, error: String) -> Container<'
             Button::new(
                 Row::new()
                     .push(
-                        Container::new(text::p1_bold(message.to_owned()).style(color::WHITE))
+                        Container::new(text::p1_bold(message.to_owned()).style(color::LIGHT_BLACK))
                             .width(Length::Fill),
                     )
                     .push(
                         Row::new()
                             .align_items(Alignment::Center)
                             .spacing(10)
-                            .push(text::p1_bold("Learn more").style(color::WHITE))
-                            .push(icon::collapsed_icon()),
+                            .push(text::p1_bold("Learn more").style(color::LIGHT_BLACK))
+                            .push(icon::collapsed_icon().style(color::LIGHT_BLACK)),
                     ),
             )
             .style(theme::Button::Transparent)
@@ -49,5 +56,88 @@ pub fn warning<'a, T: 'a + Clone>(message: String, error: String) -> Container<'
     )))
     .padding(15)
     .style(theme::Container::Card(theme::Card::Warning))
+    .width(Length::Fill)
+}
+
+pub fn processing_hardware_wallet<'a, T: 'a, K: Display, V: Display, F: Display>(
+    kind: K,
+    version: Option<V>,
+    fingerprint: F,
+    alias: Option<impl Into<Cow<'a, str>>>,
+) -> Container<'a, T> {
+    container(
+        row(vec![
+            column(vec![
+                Row::new()
+                    .spacing(5)
+                    .push_maybe(alias.map(|a| text::p1_bold(a)))
+                    .push(text::p1_regular(format!("#{}", fingerprint)))
+                    .into(),
+                Row::new()
+                    .spacing(5)
+                    .push(text::caption(kind.to_string()))
+                    .push_maybe(version.map(|v| text::caption(v.to_string())))
+                    .into(),
+            ])
+            .width(Length::Fill)
+            .into(),
+            column(vec![
+                text::p2_regular("Processing...").into(),
+                text::p2_regular("Please check your device").into(),
+            ])
+            .into(),
+        ])
+        .align_items(Alignment::Center),
+    )
+    .style(theme::Container::Notification(theme::Notification::Pending))
+    .padding(10)
+}
+
+pub fn processing_hardware_wallet_error<'a, T: 'a + Clone>(
+    message: String,
+    error: String,
+) -> Container<'a, T> {
+    let message_clone = message.clone();
+    Container::new(Container::new(collapse::Collapse::new(
+        move || {
+            Button::new(
+                Row::new()
+                    .push(
+                        Container::new(
+                            text::p1_bold(message_clone.to_string()).style(color::LIGHT_BLACK),
+                        )
+                        .width(Length::Fill),
+                    )
+                    .push(
+                        Row::new()
+                            .align_items(Alignment::Center)
+                            .spacing(10)
+                            .push(text::p1_bold("Learn more").style(color::LIGHT_BLACK))
+                            .push(icon::collapse_icon().style(color::LIGHT_BLACK)),
+                    ),
+            )
+            .style(theme::Button::Transparent)
+        },
+        move || {
+            Button::new(
+                Row::new()
+                    .push(
+                        Container::new(text::p1_bold(message.to_owned()).style(color::LIGHT_BLACK))
+                            .width(Length::Fill),
+                    )
+                    .push(
+                        Row::new()
+                            .align_items(Alignment::Center)
+                            .spacing(10)
+                            .push(text::p1_bold("Learn more").style(color::LIGHT_BLACK))
+                            .push(icon::collapsed_icon().style(color::LIGHT_BLACK)),
+                    ),
+            )
+            .style(theme::Button::Transparent)
+        },
+        move || Element::<'a, T>::from(text::p2_regular(error.to_owned())),
+    )))
+    .padding(10)
+    .style(theme::Container::Notification(theme::Notification::Error))
     .width(Length::Fill)
 }
