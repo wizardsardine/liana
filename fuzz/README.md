3017b88e27849830530fc0ccf77df215889d17ba
Antoine Poinsot <darosior@protonmail.com>
2024-03-13 11:35:27 +0100

    fuzz: fuzzing integrations, fuzz the descriptors module
    This integrates fuzzing into our project by introducing two targets
    which exercise the descriptor parsing and analysis logic.
    
    The `descriptor_parse` is dead simple but not very effective. The
    `descriptors` harness tries to be smarter by almost always generating a
    valid Liana descriptor.
    
    Of course, this is just a first integration and both could be made more
    effective.

---
+++ fuzz/README.md
@@ -0,0 +1,5 @@
+# Liana fuzzing targets
+
+This folder hosts the fuzz targets for Liana. We simply use
+[`cargo-fuzz`](https://github.com/rust-fuzz/cargo-fuzz) for now (see there for instructions on how
+to run the targets).
---
+++ fuzz/fuzz_targets/descriptor_parse.rs
@@ -0,0 +1,76 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+
+use liana::{descriptors::LianaDescriptor, miniscript::bitcoin::Network};
+use secp256k1::global::SECP256K1;
+
+use std::str::{self, FromStr};
+
+// Hacky way to detect too deeply nested wrappers, which would make rust-miniscript stack overflow
+// when parsing them.
+fn too_deep_wrappers(desc_str: &str) -> bool {
+    let mut wrapper_count = 0;
+    for c in desc_str.chars() {
+        if c == ':' || c == '(' || c == ')' || c == ',' {
+            if c == ':' && wrapper_count > 10 {
+                return true;
+            }
+            wrapper_count = 0;
+        } else {
+            wrapper_count += 1;
+        }
+    }
+    false
+}
+
+fuzz_target!(|data: &[u8]| {
+    let desc_str = match str::from_utf8(data) {
+        Ok(s) => s,
+        Err(_) => return,
+    };
+    if data.len() > 10_000 {
+        return;
+    }
+
+    // Rust-miniscript uses a recursive parsing algorithm which could make us crash when trying to
+    // parse nested fragments. Until they fix it, rule out the most trivial recursion overflow the
+    // fuzzer can generate: too deep wrappers.
+    // FIXME: this shouldn't be necessary when upgrading to the next rust-miniscript version.
+    if too_deep_wrappers(&desc_str) {
+        return;
+    }
+
+    let desc = match LianaDescriptor::from_str(desc_str) {
+        Ok(d) => d,
+        Err(_) => return,
+    };
+
+    // The descriptor must roundtrip.
+    assert_eq!(desc, LianaDescriptor::from_str(&desc.to_string()).unwrap());
+
+    // We can get the policy out of this desc and a desc out of this policy, but it's not
+    // guaranteed to roundtrip: policy->descriptor involves a compilation.
+    LianaDescriptor::new(desc.policy());
+
+    // Exercise the various methods on the descriptor. None should crash.
+    desc.receive_descriptor();
+    desc.change_descriptor();
+    desc.first_timelock_value();
+    desc.max_sat_weight();
+    desc.max_sat_vbytes();
+    desc.spender_input_size();
+
+    // Exercise the various methods of derived descriptors. None should crash.
+    let der_index = 42.into();
+    let der_descs = [
+        desc.receive_descriptor().derive(der_index, &SECP256K1),
+        desc.change_descriptor().derive(der_index, &SECP256K1),
+    ];
+    for desc in der_descs {
+        desc.address(Network::Bitcoin);
+        desc.script_pubkey();
+        desc.witness_script();
+        desc.bip32_derivations();
+    }
+});
