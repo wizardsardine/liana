0fbba9eaa24bc4fbb0966235415eba1459b7d8bd
edouardparis <m@edouard.paris>
2024-08-09 12:51:00 +0200

    Add backend section to settings to send invitation

--- gui/src/app/state/settings/mod.rs
+++ gui/src/app/state/settings/mod.rs
@@ -7,13 +7,20 @@ use std::sync::Arc;
 
 use iced::Command;
 
-use liana_ui::widget::Element;
+use liana_ui::{component::form, widget::Element};
 
 use bitcoind::BitcoindSettingsState;
 use wallet::WalletSettingsState;
 
 use crate::{
-    app::{cache::Cache, error::Error, message::Message, state::State, view, wallet::Wallet},
+    app::{
+        cache::Cache,
+        error::Error,
+        message::Message,
+        state::State,
+        view::{self},
+        wallet::Wallet,
+    },
     daemon::{Daemon, DaemonBackend},
 };
 
@@ -65,6 +72,12 @@ impl State for SettingsState {
                     .as_mut()
                     .map(|s| s.reload(daemon, wallet))
                     .unwrap_or_else(Command::none)
+            }
+            Message::View(view::Message::Settings(
+                view::SettingsMessage::EditRemoteBackendSettings,
+            )) => {
+                self.setting = Some(BackendSettingsState::new().into());
+                Command::none()
             }
             Message::View(view::Message::Settings(view::SettingsMessage::AboutSection)) => {
                 self.setting = Some(AboutSettingsState::default().into());
@@ -138,19 +151,6 @@ pub struct AboutSettingsState {
     warning: Option<Error>,
 }
 
-impl AboutSettingsState {
-    pub fn new(daemon_is_external: bool) -> Self {
-        AboutSettingsState {
-            daemon_version: if !daemon_is_external {
-                Some(liana::VERSION.to_string())
-            } else {
-                None
-            },
-            warning: None,
-        }
-    }
-}
-
 impl State for AboutSettingsState {
     fn view<'a>(&'a self, cache: &'a Cache) -> Element<'a, view::Message> {
         view::settings::about_section(cache, self.warning.as_ref(), self.daemon_version.as_ref())
@@ -158,13 +158,19 @@ impl State for AboutSettingsState {
 
     fn update(
         &mut self,
-        _daemon: Arc<dyn Daemon + Sync + Send>,
+        daemon: Arc<dyn Daemon + Sync + Send>,
         _cache: &Cache,
         message: Message,
     ) -> Command<Message> {
         if let Message::Info(res) = message {
             match res {
-                Ok(info) => self.daemon_version = Some(info.version),
+                Ok(info) => {
+                    if daemon.backend() == DaemonBackend::RemoteBackend {
+                        self.daemon_version = None;
+                    } else {
+                        self.daemon_version = Some(info.version)
+                    }
+                }
                 Err(e) => self.warning = Some(e),
             }
         }
@@ -186,6 +192,97 @@ impl State for AboutSettingsState {
 
 impl From<AboutSettingsState> for Box<dyn State> {
     fn from(s: AboutSettingsState) -> Box<dyn State> {
+        Box::new(s)
+    }
+}
+
+#[derive(Default)]
+pub struct BackendSettingsState {
+    email_form: form::Value<String>,
+    processing: bool,
+    success: bool,
+    warning: Option<Error>,
+}
+
+impl BackendSettingsState {
+    pub fn new() -> Self {
+        Self {
+            email_form: form::Value::default(),
+            processing: false,
+            success: false,
+            warning: None,
+        }
+    }
+}
+
+impl State for BackendSettingsState {
+    fn view<'a>(&'a self, cache: &'a Cache) -> Element<'a, view::Message> {
+        view::settings::remote_backend_section(
+            cache,
+            &self.email_form,
+            self.processing,
+            self.success,
+            self.warning.as_ref(),
+        )
+    }
+
+    fn update(
+        &mut self,
+        daemon: Arc<dyn Daemon + Sync + Send>,
+        _cache: &Cache,
+        message: Message,
+    ) -> Command<Message> {
+        match message {
+            Message::View(view::Message::Settings(
+                view::SettingsMessage::RemoteBackendSettings(message),
+            )) => match message {
+                view::RemoteBackendSettingsMessage::SendInvitation => {
+                    if !self.email_form.valid {
+                        return Command::none();
+                    }
+                    let email = self.email_form.value.clone();
+                    self.processing = true;
+                    self.success = false;
+                    self.warning = None;
+                    Command::perform(
+                        async move {
+                            daemon.send_wallet_invitation(&email).await?;
+                            Ok(())
+                        },
+                        Message::Updated,
+                    )
+                }
+                view::RemoteBackendSettingsMessage::EditInvitationEmail(email) => {
+                    if !self.processing {
+                        self.email_form.valid = email_address::EmailAddress::parse_with_options(
+                            &email,
+                            email_address::Options::default().with_required_tld(),
+                        )
+                        .is_ok();
+                        self.email_form.value = email;
+                        self.success = false;
+                    }
+                    Command::none()
+                }
+            },
+            Message::Updated(res) => {
+                self.processing = false;
+                match res {
+                    Ok(()) => self.success = true,
+                    Err(e) => {
+                        self.success = false;
+                        self.warning = Some(e);
+                    }
+                }
+                Command::none()
+            }
+            _ => Command::none(),
+        }
+    }
+}
+
+impl From<BackendSettingsState> for Box<dyn State> {
+    fn from(s: BackendSettingsState) -> Box<dyn State> {
         Box::new(s)
     }
 }
