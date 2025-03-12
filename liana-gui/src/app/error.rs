7e7ace8873a7b799e37a7797c1d97fcb6ae01667
pythcoiner <pythcoiner@proton.me>
2025-03-12 03:43:57 +0100

    gui: display export error

--- liana-gui/src/app/error.rs
+++ liana-gui/src/app/error.rs
@@ -60,7 +60,7 @@ impl std::fmt::Display for Error {
             Self::Unexpected(e) => write!(f, "Unexpected error: {}", e),
             Self::HardwareWallet(e) => write!(f, "error: {}\nPlease check if the device is still connected and unlocked with the correct firmware open for the current network and no other application is accessing the device.", e),
             Self::Desc(e) => write!(f, "Liana descriptor error: {}", e),
-            Self::ImportExport(e) => write!(f, "{e:?}"),
+            Self::ImportExport(e) => write!(f, "{e}"),
         }
     }
 }
