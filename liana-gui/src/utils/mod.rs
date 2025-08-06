e5fb9bdb0599caae94db61719ce983008b2c1a04
Michael Mallan <michaelmallan@proton.me>
2025-08-06 09:45:55 +0100

    refactor: add utils to get time since epoch

--- liana-gui/src/utils/mod.rs
+++ liana-gui/src/utils/mod.rs
@@ -1,3 +1,5 @@
+use std::time::{Duration, SystemTime, SystemTimeError, UNIX_EPOCH};
+
 pub mod serde;
 
 #[cfg(test)]
@@ -5,3 +7,13 @@ pub mod sandbox;
 
 #[cfg(test)]
 pub mod mock;
+
+/// Returns the current time as a [`Duration`] since the UNIX epoch.
+pub fn now() -> Duration {
+    now_fallible().expect("cannot fail")
+}
+
+/// Faliible version of [`now`].
+pub fn now_fallible() -> Result<Duration, SystemTimeError> {
+    SystemTime::now().duration_since(UNIX_EPOCH)
+}
