ca9439753124688d3b2732134bdb86a526150cb2
sokorototo <nyachiengatoto@gmail.com>
2025-08-12 14:55:47 +0300

    removed minimal padding

--- liana-gui/src/app/view/mod.rs
+++ liana-gui/src/app/view/mod.rs
@@ -154,7 +154,6 @@ pub fn sidebar<'a>(menu: &Menu, cache: &'a Cache) -> Container<'a, Message> {
                 Column::new()
                     .push(
                         Container::new(liana_logotype().width(Length::Shrink))
-                            .padding(10)
                             .align_x(iced::Alignment::Center)
                             .width(Length::Fill)
                             .style(|_| {
