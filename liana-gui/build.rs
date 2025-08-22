a5387e8920bc42e52cbc4e944ec252530149ce2a
sokorototo <nyachiengatoto@gmail.com>
2025-08-22 15:08:26 +0300

    fix CI

--- liana-gui/build.rs
+++ liana-gui/build.rs
@@ -1,7 +1,6 @@
-use winres::WindowsResource;
-
 fn main() {
-    WindowsResource::new()
+    #[cfg(windows)]
+    winres::WindowsResource::new()
         .set_icon("../liana-ui/static/logos/liana-app-icon-coincube.ico")
         .compile()
         .unwrap();
