33a50265bc2a9de6b3e4c8150d20fd29af981306
pythcoiner <pythcoiner@proton.me>
2026-01-12 16:57:06 +0000

    liana-gui: add get_api_url() in liana-connect service

--- liana-gui/src/services/connect/client/mod.rs
+++ liana-gui/src/services/connect/client/mod.rs
@@ -6,8 +6,20 @@ use liana::miniscript::bitcoin;
 
 use serde::Deserialize;
 
-const LIANALITE_SIGNET_URL: &str = "https://api.signet.lianalite.com";
-const LIANALITE_MAINNET_URL: &str = "https://api.lianalite.com";
+const DEFAULT_CONNECT_SIGNET_URL: &str = "https://api.connect.signet.lianawallet.com";
+const DEFAULT_CONNECT_MAINNET_URL: &str = "https://api.connect.lianawallet.com";
+
+/// Get Liana Lite API URL for the given network.
+/// Environment variables can override the defaults for local testing:
+/// - LIANA_CONNECT_SIGNET_API_URL: overrides only for signet/testnet
+fn get_api_url(network: bitcoin::Network) -> String {
+    if network == bitcoin::Network::Bitcoin {
+        DEFAULT_CONNECT_MAINNET_URL.to_string()
+    } else {
+        std::env::var("LIANA_CONNECT_SIGNET_API_URL")
+            .unwrap_or_else(|_| DEFAULT_CONNECT_SIGNET_URL.to_string())
+    }
+}
 
 #[derive(Debug, Clone, Deserialize)]
 struct ServiceConfigResource {
@@ -25,11 +37,7 @@ pub struct ServiceConfig {
 pub async fn get_service_config(
     network: bitcoin::Network,
 ) -> Result<ServiceConfig, reqwest::Error> {
-    let backend_api_url = if network == bitcoin::Network::Bitcoin {
-        LIANALITE_MAINNET_URL
-    } else {
-        LIANALITE_SIGNET_URL
-    };
+    let backend_api_url = get_api_url(network);
     let client = reqwest::Client::new();
     let res: ServiceConfigResource = client
         .get(format!("{}/v1/desktop", backend_api_url))
@@ -41,6 +49,6 @@ pub async fn get_service_config(
     Ok(ServiceConfig {
         auth_api_url: res.auth_api_url,
         auth_api_public_key: res.auth_api_public_key,
-        backend_api_url: backend_api_url.to_string(),
+        backend_api_url,
     })
 }
