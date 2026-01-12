bff69b1df3f810820f6c60948dbbd399dfdfd508
pythcoiner <pythcoiner@proton.me>
2026-01-12 06:56:26 +0000

    liana-business: rework main

--- liana-business/src/main.rs
+++ liana-business/src/main.rs
@@ -6,24 +6,22 @@
 
 #![windows_subsystem = "windows"]
 
-use std::{error::Error, io::Write, str::FromStr};
+use std::{error::Error, io::Write};
 
-#[cfg(target_os = "linux")]
-use iced::window::settings::PlatformSpecific;
-use iced::{Settings, Size};
+use iced::Size;
 use tracing::error;
-use tracing_subscriber::filter::LevelFilter;
 
 use liana::miniscript::bitcoin;
-use liana_ui::{component::text, font, image, theme};
+use liana_ui::theme;
 
 use business_installer::{BusinessInstaller, Message};
 use business_settings::BusinessSettings;
 use liana_gui::{
-    app::settings::global::{GlobalSettings, WindowConfig},
-    dir::LianaDirectory,
-    gui::{Config, GUI},
-    utils::args::{parse_args, Arg},
+    gui::GUI,
+    utils::{
+        app::{args_to_config, create_app_settings, create_window_settings, load_initial_size, parse_log_level},
+        args::parse_args,
+    },
     VERSION,
 };
 
@@ -46,73 +44,14 @@ fn main() -> Result<(), Box<dyn Error>> {
         Some(default_network),
     )?;
 
-    let config = match args.as_slice() {
-        [] => {
-            let datadir_path = LianaDirectory::new_default().unwrap();
-            Config::new(datadir_path, Some(default_network))
-        }
-        [Arg::Network(network)] => {
-            let datadir_path = LianaDirectory::new_default().unwrap();
-            Config::new(datadir_path, Some(*network))
-        }
-        [Arg::DatadirPath(datadir_path)] => {
-            Config::new(datadir_path.clone(), Some(default_network))
-        }
-        [Arg::DatadirPath(datadir_path), Arg::Network(network)]
-        | [Arg::Network(network), Arg::DatadirPath(datadir_path)] => {
-            Config::new(datadir_path.clone(), Some(*network))
-        }
-        _ => {
-            return Err("Unknown args combination".into());
-        }
-    };
-
-    let log_level = if let Ok(l) = std::env::var("LOG_LEVEL") {
-        Some(LevelFilter::from_str(&l)?)
-    } else {
-        None
-    };
+    let config = args_to_config(&args, Some(default_network))?;
+    let log_level = parse_log_level()?;
 
     setup_panic_hook();
 
-    let settings = Settings {
-        id: Some("LianaBusiness".to_string()),
-        antialiasing: false,
-        default_text_size: text::P1_SIZE.into(),
-        default_font: liana_ui::font::REGULAR,
-        fonts: font::load(),
-    };
-
-    let global_config_path = GlobalSettings::path(&config.liana_directory);
-    let initial_size = if let Some(WindowConfig { width, height }) =
-        GlobalSettings::load_window_config(&global_config_path)
-    {
-        Size { width, height }
-    } else {
-        // Default size for liana-business (larger than liana-gui default)
-        Size::new(1200.0, 800.0)
-    };
-
-    #[allow(unused_mut)]
-    let mut window_settings = iced::window::Settings {
-        size: initial_size,
-        icon: Some(image::liana_app_icon()), // TODO: Use custom liana-business icon
-        position: iced::window::Position::Default,
-        min_size: Some(Size {
-            width: 1000.0,
-            height: 650.0,
-        }),
-        exit_on_close_request: false,
-        ..Default::default()
-    };
-
-    #[cfg(target_os = "linux")]
-    {
-        window_settings.platform_specific = PlatformSpecific {
-            application_id: "LianaBusiness".to_string(),
-            ..Default::default()
-        };
-    }
+    let settings = create_app_settings("LianaBusiness");
+    let initial_size = load_initial_size(&config.liana_directory, Some(Size::new(1200.0, 800.0)));
+    let window_settings = create_window_settings("LianaBusiness", initial_size);
 
     if let Err(e) = iced::application(
         LianaBusiness::title,
