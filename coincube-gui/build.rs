d03e0f27f2ae6cbef49f3b518f905ec3c7a84750
sokorototo <nyachiengatoto@gmail.com>
2026-01-22 14:39:20 +0300

    ...

--- coincube-gui/build.rs
+++ coincube-gui/build.rs
@@ -4,8 +4,13 @@ fn main() {
     if cfg!(debug_assertions) {
         // Load the .env file from the project root
         if dotenv().is_ok() {
-            for (key, value) in dotenvy::vars() {
-                println!("cargo:rustc-env={}={}", key, value);
+            let env = dotenvy::dotenv_iter();
+            if let Ok(iter) = env {
+                for r in iter {
+                    if let Ok((key, value)) = r {
+                        println!("cargo:rustc-env={}={}", key, value);
+                    }
+                }
             }
         };
     }
