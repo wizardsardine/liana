06c0e744b226ee7d80217786fbfc90504eecb04f
pythcoiner <pythcoiner@proton.me>
2026-05-05 22:12:10 -0400

    gui: add debug page for hw signing devices

--- liana-gui/src/debug/mod.rs
+++ liana-gui/src/debug/mod.rs
@@ -18,6 +18,7 @@ use liana_ui::{component::text, theme, widget::*};
 
 pub mod badges;
 pub mod buttons;
+pub mod hw;
 pub mod texts;
 
 /// Every registered debug page. Append `&module::ENTRY` here when adding a new
@@ -30,6 +31,8 @@ pub const PAGES: &[&DebugPageEntry] = &[
     &buttons::ENTRY_CONSTRUCTORS_THEMED,
     &buttons::ENTRY_CONSTRUCTORS_WIDTHS,
     &buttons::ENTRY_CONSTRUCTORS_HELPERS,
+    &hw::ENTRY_PAGE_1,
+    &hw::ENTRY_PAGE_2,
     &texts::ENTRY_CONSTRUCTORS,
     &texts::ENTRY_THEMES,
     // <- add new entry here
