4b379fc4cfad76cb9193c697c2f904faded7e4fb
pythcoiner <pythcoiner@proton.me>
2026-05-06 05:59:48 +0200

    liana-ui: refactor badge components

--- liana-ui/src/component/badge.rs
+++ liana-ui/src/component/badge.rs
@@ -1,43 +1,35 @@
-use iced::Length;
-
 use crate::{icon, image, theme, widget::*};
 
+const BADGE_SIZE: u32 = 40;
+const ICON_SIZE: u32 = BADGE_SIZE / 2;
+const LIANA_ICON_SIZE: u32 = 25;
+
 pub fn badge<T>(icon: crate::widget::Text<'static>) -> Container<'static, T> {
-    Container::new(icon.width(Length::Fixed(20.0)))
+    Container::new(icon.width(ICON_SIZE))
         .style(theme::badge::simple)
-        .center_x(Length::Fixed(40.0))
-        .center_y(Length::Fixed(40.0))
+        .center_x(BADGE_SIZE)
+        .center_y(BADGE_SIZE)
 }
 
-pub fn receive<T>() -> Container<'static, T> {
-    Container::new(icon::receive_icon().width(Length::Fixed(20.0)))
-        .style(theme::badge::simple)
-        .style(theme::badge::simple)
-        .center_x(Length::Fixed(40.0))
-        .center_y(Length::Fixed(40.0))
+macro_rules! icon_badge {
+    ($name:ident, $icon:ident) => {
+        pub fn $name<T>() -> Container<'static, T> {
+            badge(icon::$icon())
+        }
+    };
 }
 
-pub fn cycle<T>() -> Container<'static, T> {
-    Container::new(icon::arrow_repeat().width(Length::Fixed(20.0)))
-        .style(theme::badge::simple)
-        .center_x(Length::Fixed(40.0))
-        .center_y(Length::Fixed(40.0))
-}
-
-pub fn spend<T>() -> Container<'static, T> {
-    Container::new(icon::send_icon().width(Length::Fixed(20.0)))
-        .style(theme::badge::simple)
-        .center_x(Length::Fixed(40.0))
-        .center_y(Length::Fixed(40.0))
-}
+icon_badge!(receive, receive_icon);
+icon_badge!(cycle, arrow_repeat);
+icon_badge!(spend, send_icon);
 
 pub fn coin<T>() -> Container<'static, T> {
     Container::new(
         image::liana_grey_logo()
-            .height(Length::Fixed(25.0))
-            .width(Length::Fixed(25.0)),
+            .height(LIANA_ICON_SIZE)
+            .width(LIANA_ICON_SIZE),
     )
     .style(theme::badge::simple)
-    .center_x(Length::Fixed(40.0))
-    .center_y(Length::Fixed(40.0))
+    .center_x(BADGE_SIZE)
+    .center_y(BADGE_SIZE)
 }
