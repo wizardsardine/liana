2d66466c7286389b0fd4fedfd585ecb6267b016c
pythcoiner <pythcoiner@proton.me>
2026-05-29 03:38:37 +0200

    liana-ui: add edit_label_modal() component

--- liana-ui/src/component/label.rs
+++ liana-ui/src/component/label.rs
@@ -1,14 +1,19 @@
 use std::fmt::Display;
 
-use iced::{widget::row, Alignment};
+use iced::{
+    widget::{column, row, Space},
+    Alignment,
+};
 
 use crate::{
-    component::{
-        button,
-        text::new::{self, H2_SPEC},
-    },
-    icon,
-    widget::Element,
+    component::text::new::{self},
+    widget::{Element, SpaceExt},
+};
+
+use super::{
+    button::{btn_edit, btn_ok},
+    form::{Form, Value},
+    modal::modal_view,
 };
 
 pub fn editable_label<'a, M: 'a + Clone>(label: impl Display, msg: M) -> Element<'a, M> {
@@ -16,12 +21,33 @@ pub fn editable_label<'a, M: 'a + Clone>(label: impl Display, msg: M) -> Element
     if label.is_empty() {
         label = "(No label)".to_string();
     }
-    let icon = icon::edit_icon().size(H2_SPEC.size.expect("size"));
-    let edit = button::flat(Some(icon), "").on_press(msg);
+    let edit = btn_edit(Some(msg));
     let label = new::h2(label);
     row![label, edit]
-        .spacing(12)
-        .padding(5)
+        .spacing(10)
         .align_y(Alignment::Center)
         .into()
+}
+
+pub fn edit_label_modal<'a, M: 'a + Clone, C>(
+    title: impl Display,
+    descr: impl Display,
+    value: &Value<String>,
+    on_change: C,
+    confirm: M,
+    close: M,
+) -> Element<'a, M>
+where
+    C: 'static + Fn(String) -> M,
+{
+    let input = Form::new(descr, value, on_change).on_submit(confirm.clone());
+    let btn_row = row![Space::fill_width(), btn_ok(Some(confirm))];
+    let content = column![input, btn_row].spacing(28);
+    modal_view(
+        Some(title),
+        None,
+        Some(close),
+        super::modal::ModalWidth::M,
+        content,
+    )
 }
