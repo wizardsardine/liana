5844ea99dd8ddc94f47ea95bc92a5272523fd194
pythcoiner <pythcoiner@proton.me>
2026-08-31 09:21:25 +0000

    gui: rework psbts view

--- liana-gui/src/app/view/psbts.rs
+++ liana-gui/src/app/view/psbts.rs
@@ -7,8 +7,9 @@ use liana_ui::{
     component::{
         button::{btn_import, btn_new},
         panels::psbts::{self, PsbtSigs},
-        text::legacy::panel_title,
+        text::new,
     },
+    spacing::{HSpacing, VSpacing},
     widget::*,
 };
 
@@ -17,24 +18,21 @@ use crate::{app::menu::Menu, daemon::model::SpendTx};
 use super::message::*;
 
 pub fn psbts_view(spend_txs: &[SpendTx], available_width: f32) -> Element<'_, Message> {
-    let title = Container::new(panel_title(Menu::PSBTs.title())).width(Length::Fill);
+    let title = Container::new(new::d2(Menu::PSBTs.title())).width(Length::Fill);
     let import = btn_import(Some(Message::ImportPsbt));
     let new_tx = btn_new(Some(Message::Menu(Menu::CreateSpendTx)));
     let header = row![title, import, new_tx]
         .align_y(Alignment::Center)
-        .spacing(10);
+        .spacing(HSpacing::M);
 
     let list = spend_txs
         .iter()
         .enumerate()
-        .fold(Column::new().spacing(10), |col, (i, tx)| {
+        .fold(Column::new().spacing(VSpacing::M), |col, (i, tx)| {
             col.push(psbt_list_entry(i, tx, available_width))
         });
 
-    column![header, list]
-        .align_x(Alignment::Center)
-        .spacing(25)
-        .into()
+    column![header, list].spacing(VSpacing::XL).into()
 }
 
 fn psbt_list_entry(i: usize, tx: &SpendTx, available_width: f32) -> Element<'_, Message> {
