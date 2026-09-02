163ea158940c45b3e7a1f693fc16fdce91278b24
pythcoiner <pythcoiner@proton.me>
2026-09-02 11:29:52 +0000

    i18n: add the Fluent runtime

--- nix/liana.nix
+++ nix/liana.nix
@@ -6,6 +6,7 @@ let
       fileset = lib.fileset.unions [
         (craneLib.fileset.commonCargoSources rootPath)
         (lib.fileset.maybeMissing (rootPath + "/liana-ui/static"))
+        (rootPath + "/liana-i18n/i18n")
       ];
     };
     strictDeps = true;
